3d7fa752448c320e810e8cf9bfa0364f438f27d6
Blujaytech <blujaytechnlogies@gmail.com>
2026-09-05 11:43:36 +0530

    roboshop-infra-code-enhanced

---
+++ 20-bastion/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.37.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "81s-remote-state-dev-hyd"
+    key    = "roboshop-bastion"
+    region = "us-east-1"
+    dynamodb_table = "81s-locking-dev-hyd"
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
---
+++ 30-rds/variables.tf
@@ -0,0 +1,23 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "common_tags" {
+    default = {
+        Project = "expense"
+        Environment = "dev"
+        Terraform = "true"
+    }
+}
+
+variable "zone_id" {
+    default = "Z00916842MCDX0S5FWPWY"
+}
+
+variable "domain_name" {
+    default = "hariawsdevops.online"
+}
